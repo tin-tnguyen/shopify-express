82e838e214f7b290ed4324e5d08a5bd5d970b2d0
Tin Nguyen <tin.trung@teletracnavman.com>
2017-12-17 01:06:42 +0800

    update

--- routes/index.js
+++ routes/index.js
@@ -8,6 +8,9 @@ const shopifyApiProxy = require('./shopifyApiProxy');
 module.exports = function createRouter(shopifyConfig) {
   const router = express.Router();
   const rawParser = bodyParser.raw({ type: '*/*' });
+  router.use(bodyParser.urlencoded({
+      extended: true
+  }));
   router.use(bodyParser.json());
 
   router.use('/auth/shopify', rawParser, createShopifyAuthRouter(shopifyConfig));
